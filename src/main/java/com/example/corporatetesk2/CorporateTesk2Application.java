a71ee76e9e8b1ba04f243d635ea34d5437e7190b
junghwan <jhzzang97@gmail.com>
2024-12-17 12:59:44 +0900

    first commit

---
+++ src/main/java/com/example/corporatetesk2/CorporateTesk2Application.java
@@ -0,0 +1,13 @@
+package com.example.corporatetesk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateTesk2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateTesk2Application.class, args);
+    }
+
+}
